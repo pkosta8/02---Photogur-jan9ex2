543cb3770186ad7c762fbbd1e8d496f7ff23e4a2
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-09 17:21:18 -0500

    Create route,  controller and view for http://localhost:3000/pictures db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pictures' => 'pictures#index'
 
-end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    get 'pictures' => 'pictures#index'
+
+    post 'pictures' => 'pictures#create'
+    get 'pictures/new' => 'pictures#new'
+
+    get 'pictures/:id' => 'pictures#show'
+  end
