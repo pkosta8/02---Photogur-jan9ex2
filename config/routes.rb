bb0e44f91ccb741c4d43bbf34f0abd7325e08b72
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-09 17:25:06 -0500

    Create route,  controller and view for http://localhost:3000/rootindex

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
+    root 'pictures#index'
+     
+
     get 'pictures' => 'pictures#index'
 
     post 'pictures' => 'pictures#create'
